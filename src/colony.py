50de2115d382aedf3c55aff59ef599e6be288e8a
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-11 14:33:43 -0400

    wrote basic Colony class

---
+++ src/colony.py
@@ -0,0 +1,10 @@
+class Colony:
+    def __init__(self, screen_width: int, screen_hight: int):
+        self.rows = 
+        self.columns = 
+        self.bit_map = 
+        
+    def __calculate_rows__(self, screen_height: int):
+        return screen_height / 16
+    def __calculate_columns__(self, screen_width: int):
+       return screen_width / 16
