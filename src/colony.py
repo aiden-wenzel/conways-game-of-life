d2fcaf4f742d48cdeff3df4ca5760e6ac5a8f473
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-11 14:36:16 -0400

    colony.pyformating

--- src/colony.py
+++ src/colony.py
@@ -1,10 +1,14 @@
 class Colony:
-    def __init__(self, screen_width: int, screen_hight: int):
-        self.rows = 
-        self.columns = 
-        self.bit_map = 
-        
-    def __calculate_rows__(self, screen_height: int):
+    def __init__(self, screen_width: int, screen_height: int):
+        self.rows = self.__calculate_rows__(screen_height)
+        self.columns = self.__calculate_columns__(screen_width)
+        self.bit_map = None  # TODO: write bit_map initiation
+
+    def __calculate_rows__(self, screen_height: int) -> int:
         return screen_height / 16
-    def __calculate_columns__(self, screen_width: int):
-       return screen_width / 16
+
+    def __calculate_columns__(self, screen_width: int) -> int:
+        return screen_width / 16
+
+    def __initiate_bit_map__(self):
+        pass
