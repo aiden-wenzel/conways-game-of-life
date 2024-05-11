b0febcd357a33c86902b01d56108ce0914ebed51
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-11 14:21:43 -0400

    reformat cell.py

--- src/cell.py
+++ src/cell.py
@@ -2,5 +2,4 @@ class Cell:
     def __init__(self, row_in: int, column_in: int):
         self.is_alive: bool = False
         self.row: int = row_in
-        self.column: int = column_in 
-        
+        self.column: int = column_in
