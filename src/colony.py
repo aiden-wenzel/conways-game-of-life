dcd5d34aaa0c30457bf42c0eeba932187488fca8
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-12 16:50:56 -0400

    updated constructor

--- src/colony.py
+++ src/colony.py
@@ -1,10 +1,11 @@
 import numpy as np
 
+
 class Colony:
     def __init__(self, screen_width: int, screen_height: int):
         self.rows = self.__calculate_rows__(screen_height)
         self.columns = self.__calculate_columns__(screen_width)
-        self.bit_map = None  # TODO: write bit_map initiation
+        self.bit_map = self.__initiate_bit_map__(self.columns, self.rows)
 
     def __calculate_rows__(self, screen_height: int) -> int:
         return screen_height / 16
