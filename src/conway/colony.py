1ff7df042ce82a1297863aa0616ac38b2fffbabc
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-13 13:46:57 -0400

    changed initiate_bit_map to initiate list of cells

--- src/conway/colony.py
+++ src/conway/colony.py
@@ -1,17 +1,21 @@
-import numpy as np
-
+import cell
 
 class Colony:
     def __init__(self, screen_width: int, screen_height: int):
-        self.rows = self.__calculate_rows__(screen_height)
-        self.columns = self.__calculate_columns__(screen_width)
-        self.bit_map = self.__initiate_bit_map__(self.columns, self.rows)
+        self.rows = self.__calculate_rows(screen_height)
+        self.columns = self.__calculate_columns(screen_width)
+        self.bit_map = self.__initiate_bit_map(self.columns, self.rows)
 
-    def __calculate_rows__(self, screen_height: int) -> int:
-        return screen_height / 16
+    def __calculate_rows(self, screen_height: int) -> int:
+        return int(screen_height / 16)
 
-    def __calculate_columns__(self, screen_width: int) -> int:
-        return screen_width / 16
+    def __calculate_columns(self, screen_width: int) -> int:
+        return int(screen_width / 16)
 
-    def __initiate_bit_map__(self, columns: int, rows: int) -> np.ndarray:
-        return np.zeros((columns, rows))
+    def __initiate_bit_map(self, columns: int, rows: int) -> list[cell.Cell]:
+        bit_map = []
+        for i in range(rows):
+            for j in range(columns):
+                bit_map[i][j] = cell.Cell(i, j)
+
+        return bit_map
