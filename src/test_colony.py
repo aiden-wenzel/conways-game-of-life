6bd4a4d3a918d5fed117326dcf22e07cf4253501
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-13 15:32:26 -0400

    colony test refactor

--- src/test_colony.py
+++ src/test_colony.py
@@ -1,10 +1,18 @@
+import pytest as pt
 import conway.colony as colony
 
-screen_width = 1280
-screen_height = 720
 
-colony_1 = colony.Colony(screen_width, screen_height)
+def test_colony():
+    screen_width = 1280
+    screen_height = 720
 
-for i in range(colony_1.rows):
-    for j in range(colony_1.columns):
-        assert not colony_1.get_cell(i, j).get_is_alive()
+    colony_1 = colony.Colony(screen_width, screen_height)
+
+    for i in range(colony_1.rows):
+        for j in range(colony_1.columns):
+            assert not colony_1.get_cell(i, j).get_is_alive()
+            assert colony_1.get_cell(i, j).row == i
+            assert colony_1.get_cell(i, j).column == j
+
+
+pt.main()
