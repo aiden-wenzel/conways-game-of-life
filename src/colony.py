ec1d707d366710d0fb47873320a0eb4a0905c1ec
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-13 15:47:00 -0400

    rearranging parameters

--- src/colony.py
+++ src/colony.py
@@ -6,7 +6,7 @@ class Colony:
     def __init__(self, screen_width: int, screen_height: int):
         self.rows = self.__calculate_rows(screen_height)
         self.columns = self.__calculate_columns(screen_width)
-        self.bit_map = self.__initiate_bit_map(self.columns, self.rows)
+        self.bit_map = self.__initiate_bit_map(self.rows, self.columns)
 
     def __calculate_rows(self, screen_height: int) -> int:
         return int(screen_height / 16)
@@ -14,7 +14,7 @@ class Colony:
     def __calculate_columns(self, screen_width: int) -> int:
         return int(screen_width / 16)
 
-    def __initiate_bit_map(self, columns: int, rows: int) -> np.ndarray:
+    def __initiate_bit_map(self, rows: int, columns: int) -> np.ndarray:
         bit_map = np.zeros((rows, columns), dtype=cell.Cell)
         for i in range(rows):
             for j in range(columns):
