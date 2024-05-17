ceeb239b1418fed638e1a07639a813d21ce3626c
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-17 16:09:51 -0400

    wrote edge test

--- src/test_colony.py
+++ src/test_colony.py
@@ -107,6 +107,23 @@ def test_num_neighbors_corner_4():
     assert colony_1.find_num_alive_neighbors(3, 3) == 2
 
 
+def test_edges():
+    screen_width = 80
+    screen_height = 80
+    colony_1 = colony.Colony(screen_width, screen_height)
+
+    colony_1.resurect_cell_at(0, 0)
+    colony_1.resurect_cell_at(1, 1)
+    colony_1.resurect_cell_at(0, 2)
+    colony_1.resurect_cell_at(3, 1)
+    colony_1.resurect_cell_at(4, 1)
+    colony_1.resurect_cell_at(3, 3)
+    colony_1.resurect_cell_at(2, 4)
+
+    assert colony_1.find_num_alive_neighbors(0, 1) == 3
+    assert colony_1.find_num_alive_neighbors(2, 0) == 2
+    assert colony_1.find_num_alive_neighbors(4, 2) == 3
+    assert colony_1.find_num_alive_neighbors(3, 4) == 2
 
 
 pt.main()
