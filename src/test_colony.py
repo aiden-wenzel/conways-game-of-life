950e83ad68ec0fc880965f53c9dca43628d5c0d8
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-18 14:48:04 -0400

    refactored test_edges()

--- src/test_colony.py
+++ src/test_colony.py
@@ -115,15 +115,15 @@ def test_num_neighbors_corner_4():
 def test_edges():
     screen_width = 80
     screen_height = 80
-    colony_1 = colony.Colony(screen_width, screen_height)
+    bit_map = np.array([
+        [1, 0, 1, 0, 0],
+        [0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1],
+        [0, 1, 0, 1, 0],
+        [0, 1, 0, 0, 0]
+        ])
+    colony_1 = colony.Colony(screen_width, screen_height, bit_map)
 
-    colony_1.resurect_cell_at(0, 0)
-    colony_1.resurect_cell_at(1, 1)
-    colony_1.resurect_cell_at(0, 2)
-    colony_1.resurect_cell_at(3, 1)
-    colony_1.resurect_cell_at(4, 1)
-    colony_1.resurect_cell_at(3, 3)
-    colony_1.resurect_cell_at(2, 4)
 
     assert colony_1.find_num_alive_neighbors(0, 1) == 3
     assert colony_1.find_num_alive_neighbors(2, 0) == 2
