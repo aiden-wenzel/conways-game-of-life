deac438763cc29097e53fb3f2d896a914f4278fb
SkewedCannon <wenzel.aiden@outlook.com>
2024-05-19 18:57:52 -0400

    wrapped game in class

--- src/main.py
+++ src/main.py
@@ -1,24 +1,29 @@
 import pygame as pg
+import colony
 
-# pg setup
-pg.init()
-screen = pg.display.set_mode((1280, 720))
-clock = pg.time.Clock()
-running = True
-test_rectangle = pg.Rect(16, 16, 16, 16)
-while running:
-    # poll for events
-    # pg.QUIT event means the user clicked X to close your window
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-    pg.draw.rect(screen, "red", test_rectangle)
-    # flip() the display to put your work on screen
-    pg.display.flip()
+class Game:
+    def __init__(self, resolution: tuple, frame_rate: int) -> None:
+        pg.init()
+        self.screen = pg.display.set_mode(resolution)
+        self.clock = pg.time.Clock()
+        self.running = True 
+        self.frame_rate = frame_rate
 
-    clock.tick(10)  # limits FPS to 60
+    def main(self) -> None:
+        while self.running:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.running = False
 
-pg.quit()
+                self.screen.fill("purple")
+
+                pg.display.flip() 
+
+            self.clock.tick(self.frame_rate)
+        
+        pg.quit()
+
+
+game_of_life = Game((1280, 720), 1)
+game_of_life.main()
