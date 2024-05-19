cc2263ee7d7d299392c7ee9263f38afe9c2a8bd6
SkewedCannon <wenzel.aiden@outlook.com>
2024-05-19 18:33:44 -0400

    rect draw experiment

--- src/main.py
+++ src/main.py
@@ -1,24 +1,24 @@
-import pygame
+import pygame as pg
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
+# pg setup
+pg.init()
+screen = pg.display.set_mode((1280, 720))
+clock = pg.time.Clock()
 running = True
-
+test_rectangle = pg.Rect(16, 16, 16, 16)
 while running:
     # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    # pg.QUIT event means the user clicked X to close your window
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
-
+    pg.draw.rect(screen, "red", test_rectangle)
     # flip() the display to put your work on screen
-    pygame.display.flip()
+    pg.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(10)  # limits FPS to 60
 
-pygame.quit()
+pg.quit()
