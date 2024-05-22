3d958fcc3bf0cdbea1c7d2468b558505c098e73f
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-05-22 15:46:13 -0400

    Add documentation to utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,5 +3,9 @@ import pandas as pd
 
 
 def read_pattern(file_path: str) -> np.ndarray:
+    """
+    Return a matrix of 1's and 0's from a given txt file also containing 1's and 0's.
+    """
+
     dataframe = pd.read_csv(file_path, sep=r'\s+', header=None)
     return dataframe.to_numpy()
